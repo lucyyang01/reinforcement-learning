32c2a2c61e53540d1fdc3e27ddc7df6a9503219e
Boris <boristomov@berkeley.edu>
2023-10-04 23:48:35 -0700

    q1 done

--- reinforcement/valueIterationAgents.py
+++ reinforcement/valueIterationAgents.py
@@ -57,27 +57,28 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.discount = discount
         self.iterations = iterations
         self.values = util.Counter() # A Counter is a dict with default 0
-        #mapping from states to their qvalue
+        #stores info about best action leading to a state
+        self.actionAtState = {state: None for state in self.mdp.getStates()}
         self.runValueIteration()
 
     def runValueIteration(self):
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
-        startState = self.mdp.getStartState()
-        possible_actions = self.mdp.getPossibleActions(startState)
-    
-        
         for i in range(self.iterations):
-            for state in self.mdp.getStates():
-                currState = state
+            buffer = util.Counter()
+            for currState in self.mdp.getStates():
                 maxQValue = float('-inf')
-                # bestAction = self.computeActionFromValues(currState)
-                # print(bestAction)
-                # print("AOIEFJHWOEFHNWOEFIH", self.computeQValueFromValues(currState, bestAction))
-                # self.values[currState] = self.computeQvaluesFromValues(currState, bestAction)
+                possible_actions = self.mdp.getPossibleActions(currState)
                 for action in possible_actions:
-                    maxQValue = max(maxQValue, self.getQValue(currState, action))
-            self.values[currState] = maxQValue
+                    # maxQValue = max(maxQValue, self.getQValue(currState, action))
+                    # buffer[currState] = maxQValue
+                    # self.actionAtState[currState] = action 
+                    newQvalue = self.getQValue(currState, action)
+                    if maxQValue <  newQvalue:
+                        maxQValue = newQvalue
+                        buffer[currState] = maxQValue
+                        self.actionAtState[currState] = action 
+            self.values = buffer
             #update state
                 
                 
@@ -151,17 +152,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         #         best_action = action
         # # print("computeaction total", qValue, best_action)
         # return best_action
-        possible_actions = self.mdp.getPossibleActions(state)
-        if len(possible_actions) == 0:
-            return None
-        # maxQValue = self.getValue(state)
-        best_action = None
-        qValue = float('-inf')
-        for action in possible_actions:
-            if self.getQValue(state, action) > qValue:
-                qValue = self.getQValue(state, action)
-                best_action = action
-        return action
+        
+        return self.actionAtState[state]
 
         
 
